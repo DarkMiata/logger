543c0e88cd3d59ea386c080d4e8e1ef277cc9444
DarkMiata <samvermeulen@gmail.com>
2017-04-02 15:59:38 +0200

    gestions d'erreurs DB

--- class/drk_logger.php
+++ class/drk_logger.php
@@ -16,24 +16,49 @@ class drk_logger {
   private $DB_name;
   private $DB_login;
   private $DB_pwd;
-  private $DB_init_state;
+  private $DB_state_init; // défini si les params de co DB sont fait
+  private $DB_state_co;   // connexion à la DB réussi
 
   // ========================================
 
   private function get_DB_url() {
+    if ($this->DB_state_init == FALSE) {
+      trigger_error("get_DB_url ne peut être appelé:"
+              . " db non intialisé", E_USER_WARNING);
+    }
+    else {
     return $this->DB_url;
+    }
   }
   private function get_DB_name() {
+    if ($this->DB_state_init == FALSE) {
+      trigger_error("get_DB_name ne peut être appelé:"
+              . " db non intialisé", E_USER_WARNING);
+    }
+    else {
     return $this->DB_name;
+    }
   }
   private function get_DB_login() {
+    if ($this->DB_state_init == FALSE) {
+      trigger_error("get_DB_login ne peut être appelé:"
+              . " db non intialisé", E_USER_WARNING);
+    }
+    else {
     return $this->DB_login;
+    }
   }
-  private function get_DN_pwd() {
+  private function get_DB_pwd() {
+    if ($this->DB_state_init == FALSE) {
+      trigger_error("get_DB_pwd ne peut être appelé:"
+              . " db non intialisé", E_USER_WARNING);
+    }
+    else {
     return $this->DB_pwd;
+    }
   }
 
-  // ========================================
+    // ========================================
 
   private function set_DB($DB) {
     $this->DB = $DB;
@@ -50,15 +75,20 @@ class drk_logger {
   private function set_DB_pwd($DB_pwd) {
     $this->DB_pwd = $DB_pwd;
   }
-  function set_DB_init_state($DB_init_state) {
-    $this->DB_init_state = $DB_init_state;
+  private function set_DB_state_init($DB_state_init) {
+    $this->DB_state_init = $DB_state_init;
   }
-
+  private function set_DB_state_co($DB_state_co) {
+    $this->DB_state_co = $DB_state_co;
+    return $this;
+  }
+  
         // ========================================
 
   function __construct($name) {
     $this->name = $name;
-    $this->set_DB_init_state(FALSE);
+    $this->set_DB_state_init(FALSE);
+    $this->set_DB_state_co(FALSE);
   }
 
   // ========================================
@@ -75,7 +105,7 @@ class drk_logger {
   // ------------------------
   public function warn($text) {
 
-    if ($this->DB_init_state == false) {
+    if ($this->DB_state_init == false) {
       trigger_error("db non intialisé", E_USER_WARNING);
     }
 
@@ -97,24 +127,71 @@ class drk_logger {
   // ========================================
   // DB
 
-  private function DB_connexion() {
-    $bdd_co = 'mysql:host='  . $this->DB_url()
-            . ';dbname='     . $this->DB_name()
+  function DB_connexion() {
+    $bdd_co = 'mysql:host='  . $this->get_DB_url()
+            . ';dbname='     . $this->get_DB_name()
             . ';charset=utf8';
 
-    $bdd = new PDO($bdd_co, $this->DB_login(), $this->DB_pwd());
+    try {
+      $bdd = new PDO($bdd_co, $this->get_DB_login(), $this->get_DB_pwd());
+      $this->set_DB($bdd);
+      $this->DB_state_co = TRUE;
+      
+      return $bdd;
+    }
+    catch(Exception $e) {
+      $erTxt = "** Erreur de connexion à la DB **<br>"
+              . $e->getMessage() . "<br>"
+              . "login: '"  . $this->get_DB_login() . "'<br>"
+              . "mdp: '"    . $this->get_DB_pwd()   . "'<br>"
+              ;
+      
+      echo($erTxt);
+      $this->DB_state_co = FALSE;
+    }
 
-    $this->set_DB($bdd);
   }
   // ------------------------
-
+/**
+ * 
+ * @param type $DB_url
+ * @param type $DB_name
+ * @param type $DB_login
+ * @param type $DB_pwd
+ */
   public function DB_init($DB_url, $DB_name, $DB_login, $DB_pwd) {
     $this->set_DB_url($DB_url);
     $this->set_DB_name($DB_name);
     $this->set_DB_login($DB_login);
     $this->set_DB_pwd($DB_pwd);
-    $this->set_DB_init_state(TRUE);
+    $this->set_DB_state_init(TRUE);
+  }
+  // ------------------------
+
+  public function DB_createTable($tableName) {
+    $bdd = $this->DB_connexion();
+    
+    $reqSqlTxt =
+            "CREATE TABLE drklog_" . $tableName
+            . "("
+            . " 'id' INT PRIMARY KEY NOT NULL,"
+            . " 'message' TEXT NOT NULL,"
+            . " 'type' CHAR(4) NOT NULL,"
+            . " 'time' TIMESTAMP NOT NULL"
+            . ");"
+          ;
+
+    try {    
+      $reqSql = $bdd->query($reqSqlTxt);
+      echo "req:";
+      echo($bdd->errorInfo()[2]);
+    }
+    catch(Exception $e) {
+      echo ("erreur sql: " . $e->getMessage() . "<br>");
+    }
+
   }
 
   // ========================================
+ 
   }
--- index.php
+++ index.php
@@ -4,10 +4,13 @@ require_once 'config/config.php';
 
 $log = new drk_logger("log1");
 
-$log->DB_init("55", "55", "897", "787");
+$log->DB_init(DB_URL, DB_NAME, DB_LOGIN, DB_PWD);
 
 $log->warn("test");
 $log->warn("test2");
 
 var_dump($log);
 
+//$log->DB_connexion();
+
+$log->DB_createTable("1");
